d4b2139a76d6d525fa6c1eed11fce1c29898fd12
Javier Morales Socuellamos <jm.socuellamos@gmail.com>
2026-01-16 19:17:02 +0100

    Implement Poisson process simulation and visualization
    Added functions to simulate and visualize Poisson processes.

--- sim_cm.py
+++ sim_cm.py
@@ -112,3 +112,126 @@ def MC_estim(sims):
   ic_up = estim + cuantil*error
   # Resultado
   return([round(estim,4), round(ic_low,4), round(ic_up,4)])
+
+def generate_poisson_events(rate, time_duration):
+  """
+  La función generate_poisson_events simula un proceso de Poisson mediante la generación de eventos
+  con una tasa promedio dada (rate) durante un período de tiempo especificado (time_duration).
+
+  La función devuelve el número de eventos (num_events), los tiempos de los eventos (event_times) y
+  los tiempos entre llegadas correspondientes (inter_arrival_times), lo que proporciona una
+  representación de un proceso de Poisson.
+  """
+  num_events = np.random.poisson(rate * time_duration)
+  event_times = np.sort(np.random.uniform(0, time_duration, num_events))
+  inter_arrival_times = np.diff(event_times)
+  return num_events, event_times, inter_arrival_times
+
+
+def plot_non_sequential_poisson(num_events, event_times, inter_arrival_times, rate, time_duration):
+  """
+  La función plot_non_sequential_poisson crea una visualización de dos paneles de
+  una simulación de proceso de Poisson no secuencial. El primer panel muestra los tiempos de los eventos
+  y el segundo muestra un histograma de los tiempos entre llegadas entre eventos.
+
+  La función acepta la cantidad de eventos (num_events), los tiempos de los eventos (event_times),
+  los tiempos entre llegadas (inter_arrival_times), la tasa (rate) del proceso de Poisson y
+  la duración total de la simulación como entrada (time_duration).
+  """
+  fig, axs = plt.subplots(1, 2, figsize=(15, 6))
+  fig.suptitle(f'Poisson Process Simulation (λ = {rate}, Duration = {time_duration} seconds)\n', fontsize=16)
+
+  axs[0].step(event_times, np.arange(1, num_events + 1), where='post', color='blue')
+  axs[0].set_xlabel('Time')
+  axs[0].set_ylabel('Event Number')
+  axs[0].set_title(f'Poisson Process Event Times\nTotal: {num_events} events\n')
+  axs[0].grid(True)
+
+  axs[1].hist(inter_arrival_times, bins=20, color='green', alpha=0.5)
+  axs[1].set_xlabel('Inter-Arrival Time')
+  axs[1].set_ylabel('Frequency')
+  axs[1].set_title(f'Histogram of Inter-Arrival Times\nMEAN: {np.mean(inter_arrival_times):.2f} | STD: {np.std(inter_arrival_times):.2f}\n')
+  axs[1].grid(True, alpha=0.5)
+
+  plt.tight_layout()
+  plt.show()
+
+
+def plot_sequential_poisson(num_events_list, event_times_list, inter_arrival_times_list, rate, time_duration):
+  """
+  La función plot_sequential_poisson es responsable de generar un gráfico secuencial de eventos de procesos de Poisson
+  simulados con diferentes tasas de llegada. Crea una figura de subgráfico de 2x1 que muestra los tiempos de los eventos
+  y un histograma de los tiempos entre llegadas. Cada subgráfico muestra datos para múltiples procesos de Poisson
+  con diferentes tasas, que se distinguen por colores distintos. La función toma num_events_list, event_times_list
+  e inter_arrival_times_list como entrada, que contienen estadísticas de eventos para cada tasa, junto con la lista de
+  tasas que especifica los valores lambda y time_duration que indica la duración de la simulación.
+
+  Visualiza los datos, mostrando la ocurrencia de eventos a lo largo del tiempo y la distribución de los tiempos
+  entre llegadas, lo que ayuda a comparar y analizar procesos de Poisson con diferentes tasas de llegada en un solo gráfico.
+  """
+  fig, axs = plt.subplots(1, 2, figsize=(15, 6))
+  fig.suptitle(f'Poisson Process Simulation (Duration = {time_duration} seconds)\n', fontsize=16)
+
+  axs[0].set_xlabel('Time')
+  axs[0].set_ylabel('Event Number')
+  axs[0].set_title(f'Poisson Process Event Times')
+  axs[0].grid(True)
+
+  axs[1].set_xlabel('Inter-Arrival Time')
+  axs[1].set_ylabel('Frequency')
+  axs[1].set_title(f'Histogram of Inter-Arrival Times')
+  axs[1].grid(True, alpha=0.5)
+
+  color_palette = plt.get_cmap('tab20')
+  colors = [color_palette(i) for i in range(len(rate))]
+
+  for n, individual_rate in enumerate(rate):
+      num_events = num_events_list[n]
+      event_times = event_times_list[n]
+      inter_arrival_times = inter_arrival_times_list[n]
+
+      axs[0].step(event_times, np.arange(1, num_events + 1), where='post', color=colors[n], label=f'λ = {individual_rate}, Total Events: {num_events}')
+      axs[1].hist(inter_arrival_times, bins=20, color=colors[n], alpha=0.5, label=f'λ = {individual_rate}, MEAN: {np.mean(inter_arrival_times):.2f}, STD: {np.std(inter_arrival_times):.2f}')
+
+  axs[0].legend()
+  axs[1].legend()
+
+  plt.tight_layout()
+  plt.show()
+
+
+def poisson_simulation(rate, time_duration, show_visualization=True):
+    """
+    La función poisson_simulation simula un proceso de Poisson con una sola tasa o una lista de tasas
+    durante un período de tiempo especificado. Si se proporciona una sola tasa, genera eventos de Poisson
+    y sus tiempos entre llegadas y, opcionalmente, los visualiza. Si se proporciona una lista de tasas,
+    genera eventos de Poisson y tiempos entre llegadas para cada tasa y, opcionalmente, los visualiza
+    de forma secuencial (show_visualization = True).
+
+    La función devuelve las estadísticas de eventos número de eventos (num_events_list), tiempos de
+    eventos (event_times_list), tiempos entre llegadas (inter_arrival_times_list) cuando la visualización
+    está desactivada o None cuando la visualización está habilitada.
+    """
+    if isinstance(rate, int):
+        num_events, event_times, inter_arrival_times = generate_poisson_events(rate, time_duration)
+
+        if show_visualization:
+            plot_non_sequential_poisson(num_events, event_times, inter_arrival_times, rate, time_duration)
+        else:
+            return num_events, event_times, inter_arrival_times
+
+    elif isinstance(rate, list):
+        num_events_list = []
+        event_times_list = []
+        inter_arrival_times_list = []
+
+        for individual_rate in rate:
+            num_events, event_times, inter_arrival_times = generate_poisson_events(individual_rate, time_duration)
+            num_events_list.append(num_events)
+            event_times_list.append(event_times)
+            inter_arrival_times_list.append(inter_arrival_times)
+
+        if show_visualization:
+            plot_sequential_poisson(num_events_list, event_times_list, inter_arrival_times_list, rate, time_duration)
+        else:
+            return num_events_list, event_times_list, inter_arrival_times_list
